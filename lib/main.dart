8e69ddd8a8e9568a6f84901b800f138abdb1d146
Prakhar Verma <connect@iprakharv.com>
2024-06-01 01:26:49 +0530

    Removed debug tag

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
-import 'screen/splash_screen.dart'; // Import the splash screen file
+import 'screen/splash_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // Define the default brightness and colors.
         brightness: Brightness.dark,
         primaryColor: Colors.blueGrey[900],
-
-        // Define the default font family.
         fontFamily: 'Georgia',
       ),
-      home: const SplashScreen(), // Point to the SplashScreen widget
+      home: const SplashScreen(),
     );
   }
 }
